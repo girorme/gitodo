ece5a379911896e9264772e684771594cfd459e2
girorme <rodrigo.girorme@i9xp.com.br>
2021-02-14 23:44:08 -0300

    Add done behavior

--- lib/gitodo_web/live/todo_live.ex
+++ lib/gitodo_web/live/todo_live.ex
@@ -19,6 +19,13 @@ defmodule GitodoWeb.TodoLive do
     {:noreply, fetch(socket)}
   end
 
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{done: !todo.done})
+
+    {:noreply, socket}
+  end
+
   defp fetch(socket) do
     assign(socket, todos: Todos.list_todos())
   end
