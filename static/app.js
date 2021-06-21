d161e8cb18b8d6df5629897bacf560f47373d498
girorme <rodrigogirorme@gmail.com>
2021-06-21 02:22:19 -0300

    Add momentjs to format dates and improve labels

--- static/app.js
+++ static/app.js
@@ -23,6 +23,11 @@ var filters = {
   }
 };
 
+Vue.filter('formatDate', function(value) {
+  if (value) {
+      return moment(String(value)).format('YYYY-MM-DD hh:mm')
+  }
+});
 
 var app = new Vue({
   data: {
